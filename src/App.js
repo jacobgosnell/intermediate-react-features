e6048b86b039c30489c43af0d922bbb4679cb673
Jake <gosnelljacob@gmail.com>
2019-11-16 15:40:23 -0800

    refactored toggle component to return a jsx element that simply returns its children to have the object data, on (set to this.state.on) and toggle (referring to the toggle function, which executes its state of on to be set to the opposite of the initial false state of on) and used this component in the app to toggle a heading on the click of a button

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Toggle from './components/ToggleRenderProps';
+import Toggle from './components/ToggleRPC';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Toggle 
-          render={({on, toggle}) => (
+        <Toggle>
+          {({on, toggle}) => (
             <div>
               {on && <h1>Show Me</h1>}
               <button onClick={toggle}>Show / Hide</button>
             </div>
           )}
-        />
+        </Toggle>
         <a
           className="App-link"
           href="https://reactjs.org"

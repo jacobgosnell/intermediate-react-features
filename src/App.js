583b3fdd733a4abc7f31ce5f43bd7184a0715a7b
Jake <gosnelljacob@gmail.com>
2019-11-16 16:28:09 -0800

    built a portal component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Toggle from './components/ToggleRPC';
+import Portal from './components/Portal';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Toggle>
-          {({on, toggle}) => (
-            <div>
-              {on && <h1>Show Me</h1>}
-              <button onClick={toggle}>Show / Hide</button>
-            </div>
-          )}
-        </Toggle>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Toggle>
+            {({on, toggle}) => (
+              <Fragment>
+                {on && <h1>Show Me</h1>}
+                <button onClick={toggle}>Show / Hide</button>
+                <Portal>
+                  {on && <h1>Hi, i'm in a portal.</h1>}
+                </Portal>
+              </Fragment>
+            )}
+          </Toggle>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
-
 export default App;

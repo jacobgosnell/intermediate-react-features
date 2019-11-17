8f0e54628e74ca697dedddf4a02a6d02949603ad
Jake <gosnelljacob@gmail.com>
2019-11-17 01:07:44 -0800

    built styles for a reusable modal component with styled components

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,12 +3,40 @@ import styled from 'styled-components';
 import Portal from './Portal';
 
 const ModalWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   position: absolute;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-  background: teal;
+`;
+
+const ModalCard = styled.div`
+  position: relative;
+  background: white;
+  border-radius: 5px;
+  box-shadow: 2px 2px 10px rgba(0,0,0,0.3);
+  padding: 15px;
+  z-index: 10;
+  min-width: 320px;
+`;
+
+const CloseButton = styled.button`
+  position: absolute;
+  top: 0;
+  right: 0;
+`;
+
+const Background = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background: black;
+  opacity: 0.5;
 `;
 
 class Modal extends Component {
@@ -18,8 +46,11 @@ class Modal extends Component {
       <Portal>
         {on && (
           <ModalWrapper>
-            <button onClick={toggle}>Close</button>
-            <div>{children}</div>
+            <ModalCard>
+              <CloseButton onClick={toggle}>Close</CloseButton>
+              <div>{children}</div>
+            </ModalCard>
+            <Background onClick={toggle}/>
           </ModalWrapper>
         )}
       </Portal>
